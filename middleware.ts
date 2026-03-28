7dcfdd6fe886a670eed80744cf8410736ac2da3d
fortinenta <priomas112233@gmail.com>
2026-03-28 12:42:51 +0700

    Fi x1.0

--- middleware.ts
+++ middleware.ts
@@ -2,10 +2,6 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
-  // Hanya log request yang mengarah ke API Proxy
-  if (request.nextUrl.pathname.startsWith('/api/proxy')) {
-    console.log(`[API-HIT] ${request.method} ${request.nextUrl.pathname}`)
-  }
   return NextResponse.next()
 }
 
